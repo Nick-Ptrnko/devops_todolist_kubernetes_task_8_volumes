d147fdc775a6138fa9b57bf07314722c091358b8
Mykola Petrenko <mykola.petrenko@gmail.com>
2026-04-25 11:19:01 +0300

    Solution

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+
+echo "--- Deploying TodoApp Infrastructure ---"
+
+# 1. Створюємо оточення
+kubectl apply -f namespace.yml
+
+# 2. Створюємо сховище (PV та PVC)
+kubectl apply -f pv.yml
+kubectl apply -f pvc.yml
+
+# 3. Створюємо конфігурації
+kubectl apply -f confgiMap.yml
+kubectl apply -f secret.yml
+
+# 4. Запускаємо сервіси та застосунок
+kubectl apply -f clusterIp.yml
+kubectl apply -f nodeport.yml
+kubectl apply -f deployment.yml
+
+echo "--- Deployment finished! ---"
+kubectl get pods -n todoapp
